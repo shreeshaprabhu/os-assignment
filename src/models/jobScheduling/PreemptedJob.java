d337f5d4c3cc2c6e03d96775b59a8ab6ab6305bc
Shreesha Prabhu K <shreeshaprabhu@gmail.com>
2015-07-28 19:30:41 +0530

    Added comparison function for PreemptedJob.

--- src/models/jobScheduling/PreemptedJob.java
+++ src/models/jobScheduling/PreemptedJob.java
@@ -37,4 +37,32 @@ public class PreemptedJob extends Job
 	{
 		this.pseudoBurstTime = pseudoBurstTime;
 	}
+
+	/**
+	 * Compares PreemptedJobs based on their remaining time (i.e. pseudoBurstTime).
+	 * If they have same remaining time, then latest arrived job is preferred
+	 * as it would reduce average response time.
+	 *
+	 * @param j1 Job 1
+	 * @param j2 Job 2
+	 * @return the <code>int</code> comparing preempted-job
+	 */
+	public static int compareRemaingBurstTime(PreemptedJob j1, PreemptedJob j2)
+	{
+		if (j1.pseudoBurstTime < j2.pseudoBurstTime)
+			return -1;
+		if (j1.pseudoBurstTime > j2.pseudoBurstTime)
+			return 1;
+		if (j1.getArrivalTime() > j2.getArrivalTime())
+			return -1;
+		if (j1.getArrivalTime() < j2.getArrivalTime())
+			return 1;
+		if (j1.getId() == j2.getId())
+			return 0;
+		if (j1.getId() < j2.getId())
+			return -1;
+		if (j1.getId() > j2.getId())
+			return 1;
+		return 0;
+	}
 }
